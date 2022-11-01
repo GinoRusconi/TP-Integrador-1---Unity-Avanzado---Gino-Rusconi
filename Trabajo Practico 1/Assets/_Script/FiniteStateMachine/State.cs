2632aaf2b72b9eb2500cb9825efc87be2d98ddb8
Gino Rusconi <ginorusconi@gmail.com>
2022-11-01 14:19:19 -0300

    Add FiniteStateMachine

---
+++ Trabajo Practico 1/Assets/_Script/FiniteStateMachine/State.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class State
+{
+    protected Player player;
+    protected 
+    protected void LogicUpdate()
+    {
+
+    }
+
+    protected void PhysicsLogic()
+    {
+
+    }
+    
+}
